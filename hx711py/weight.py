fbe573e1b511203787ad89c5ceb677c7724bfaa2
dtrilsbeek <d.trilsbeek@gmail.com>
2020-01-10 14:49:39 +0100

    repeat post 5 seconds

--- hx711py/weight.py
+++ hx711py/weight.py
@@ -75,29 +75,12 @@ starttime = time.time()
 while True:
 
     try:
-        # These three lines are usefull to debug wether to use MSB or LSB in the reading formats
-        # for the first parameter of "hx.set_reading_format("LSB", "MSB")".
-        # Comment the two lines "val = hx.get_weight(5)" and "print val" and uncomment these three lines to see what it prints.
 
-        # np_arr8_string = hx.get_np_arr8_string()
-        # binary_string = hx.get_binary_string()
-        # print binary_string + " " + np_arr8_string
-
-        # Prints the weight. Comment if you're debbuging the MSB and LSB issue.
-        # Exclude negative values
         val = max(0, int(hx.get_weight(5)))
-        # Include negative values
-        # val = hx.get_weight(5)
         print("Weight: ", val)
 
-        # postWeight(val)
-        # time.sleep(5.0 - ((time.time() - starttime) % 5.0))
-
-        # To get weight from both channels (if you have load cells hooked up
-        # to both channel A and B), do something like this
-        # val_A = hx.get_weight_A(5)
-        # val_B = hx.get_weight_B(5)
-        # print "A: %s  B: %s" % ( val_A, val_B )
+        postWeight(val)
+        time.sleep(4.9 - ((time.time() - starttime) % 4.9))
 
         hx.power_down()
         hx.power_up()
